cdc1a30d5b0b042d88a3a44ffe08b8d02f2e259f
zstearman3 <zstearman@hey.com>
2022-03-29 02:21:32 +0000

    Add player test suite

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Player class contains top level information for a player
+# A player will belong to the last team which he has played for
 class Player < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -6,7 +10,7 @@ class Player < ApplicationRecord
   belongs_to :class_designation
 
   def formatted_height
-    feet = (height/12.0).floor
+    feet = (height / 12.0).floor
     inches = height % 12
     "#{feet}' #{inches}"
   end
